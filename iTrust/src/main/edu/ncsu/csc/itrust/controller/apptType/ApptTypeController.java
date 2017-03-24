4db6642a8e2b134a1dae8e5b731ad87c9006495a
ereinst <ereinst@ncsu.edu>
2017-03-24 13:22:42 -0400

    Display appointment type when viewing office visits

--- iTrust/src/main/edu/ncsu/csc/itrust/controller/apptType/ApptTypeController.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/controller/apptType/ApptTypeController.java
@@ -1,14 +1,18 @@
 package edu.ncsu.csc.itrust.controller.apptType;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import edu.ncsu.csc.itrust.exception.DBException;
 import edu.ncsu.csc.itrust.model.apptType.*;
 
 @ManagedBean(name="appt_type_controller")
 public class ApptTypeController {
+	
 	private static ApptTypeData apptTypeData;
+	
 	public ApptTypeController() throws DBException{
 		if(apptTypeData == null){
 			ApptTypeController.apptTypeData = new ApptTypeMySQLConverter();
@@ -16,7 +20,25 @@ public class ApptTypeController {
 		}
 		
 	}
-	public List<ApptType> getApptTypelList() throws DBException{
+	
+	public String getApptTypeForID(String apptTypeID) {
+		String apptType = null;
+		try {
+			List<ApptType> list = apptTypeData.getAll();
+			long id = Long.parseLong(apptTypeID);			
+			for (ApptType appt : list) {
+				if (appt.getID() == id) {
+					apptType = appt.getName();
+				}
+			}			
+		} catch (Exception e) {
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid Appointment Type ID", "Invalid Appointment Type ID");
+	        FacesContext.getCurrentInstance().addMessage(null, msg);
+		}
+		return apptType;
+	}
+	
+	public List<ApptType> getApptTypeList() throws DBException{
 		return apptTypeData.getAll();
 	}
 
