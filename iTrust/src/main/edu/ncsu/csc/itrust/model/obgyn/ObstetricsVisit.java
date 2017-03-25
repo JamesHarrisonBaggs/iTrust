308d17c8823da9f2e8de3b0036e7b4f568b7a5f6
ereinst <ereinst@ncsu.edu>
2017-03-25 18:14:21 -0400

    Added fields to ObstetricsVisit and Ultrasound Beans

--- iTrust/src/main/edu/ncsu/csc/itrust/model/obgyn/ObstetricsVisit.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/model/obgyn/ObstetricsVisit.java
@@ -1,8 +1,86 @@
 package edu.ncsu.csc.itrust.model.obgyn;
 
+import java.time.LocalDate;
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name="obstetrics_visit")
 public class ObstetricsVisit {
+	
+	/** The MID of the patient */
+	private long patientId;
+	/** ID of the associated office visit */
+	private long visitId;
+	/** The date of the office visit */
+	private LocalDate visitDate;
+	/** Number of weeks pregnant */
+	private int weeksPregnant;
+	/** The patient weight in lbs */
+	private double weight;
+	/** The blood pressure in mmHg */
+	private String bloodPressure;
+	/** The heart rate of the fetus in bpm */
+	private int fetalHeartRate;
+	/** The number of fetuses */
+	private int amount;
+	/** Boolean for if the placenta is low lying */
+	private boolean lowLyingPlacenta;
+	
+	/** GETTERS **/
+	public long getPatientId() {
+		return patientId;
+	}
+	public long getVisitId() {
+		return visitId;
+	}
+	public LocalDate getVisitDate() {
+		return visitDate;
+	}
+	public int getWeeksPregnant() {
+		return weeksPregnant;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public String getBloodPressure() {
+		return bloodPressure;
+	}
+	public int getFetalHeartRate() {
+		return fetalHeartRate;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public boolean isLowLyingPlacenta() {
+		return lowLyingPlacenta;
+	}
+	
+	/** SETTERS **/
+	public void setPatientId(long patientId) {
+		this.patientId = patientId;
+	}
+	public void setVisitId(long visitId) {
+		this.visitId = visitId;
+	}
+	public void setVisitDate(LocalDate visitDate) {
+		this.visitDate = visitDate;
+	}
+	public void setWeeksPregnant(int weeksPregnant) {
+		this.weeksPregnant = weeksPregnant;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public void setBloodPressure(String bloodPressure) {
+		this.bloodPressure = bloodPressure;
+	}
+	public void setFetalHeartRate(int fetalHeartRate) {
+		this.fetalHeartRate = fetalHeartRate;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public void setLowLyingPlacenta(boolean lowLyingPlacenta) {
+		this.lowLyingPlacenta = lowLyingPlacenta;
+	}
 	
 }
--- iTrust/src/main/edu/ncsu/csc/itrust/model/obgyn/Ultrasound.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/model/obgyn/Ultrasound.java
@@ -5,4 +5,97 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean(name="ultrasound")
 public class Ultrasound {
 	
+	/** The MID of the patient */
+	private long patientId;
+	/** ID of the associated office visit */
+	private long visitId;
+	/** ID of the fetus */
+	private int fetusId;
+	/** Crown rump length (CRL) in mm */
+	private int crownRumpLength;
+	/** Biparietal diameter (BPD) in mm */
+	private int biparietalDiameter;
+	/** Head circumference (HC) in mm */
+	private int headCircumference;
+	/** Femur length (FL) in mm */
+	private int femurLength;
+	/** Occipitofrontal diameter (OFD) in mm */
+	private int occipitofrontalDiameter;
+	/** Abdominal circumference (AC) in mm */
+	private int abdominalCircumference;
+	/** Humerus length (HL) in mm */
+	private int humerusLength;
+	/** Estimated fetal weight (grams) */
+	private int estimatedFetalWeight;
+	
+	/** GETTERS **/
+	public long getPatientId() {
+		return patientId;
+	}
+	public long getVisitId() {
+		return visitId;
+	}
+	public int getFetusId() {
+		return fetusId;
+	}
+	public int getCrownRumpLength() {
+		return crownRumpLength;
+	}
+	public int getBiparietalDiameter() {
+		return biparietalDiameter;
+	}
+	public int getHeadCircumference() {
+		return headCircumference;
+	}
+	public int getFemurLength() {
+		return femurLength;
+	}
+	public int getOccipitofrontalDiameter() {
+		return occipitofrontalDiameter;
+	}
+	public int getAbdominalCircumference() {
+		return abdominalCircumference;
+	}
+	public int getHumerusLength() {
+		return humerusLength;
+	}
+	public int getEstimatedFetalWeight() {
+		return estimatedFetalWeight;
+	}
+	public void setPatientId(long patientId) {
+		this.patientId = patientId;
+	}
+	
+	/** SETTERS **/
+	public void setVisitId(long visitId) {
+		this.visitId = visitId;
+	}
+	public void setFetusId(int fetusId) {
+		this.fetusId = fetusId;
+	}
+	public void setCrownRumpLength(int crownRumpLength) {
+		this.crownRumpLength = crownRumpLength;
+	}
+	public void setBiparietalDiameter(int biparietalDiameter) {
+		this.biparietalDiameter = biparietalDiameter;
+	}
+	public void setHeadCircumference(int headCircumference) {
+		this.headCircumference = headCircumference;
+	}
+	public void setFemurLength(int femurLength) {
+		this.femurLength = femurLength;
+	}
+	public void setOccipitofrontalDiameter(int occipitofrontalDiameter) {
+		this.occipitofrontalDiameter = occipitofrontalDiameter;
+	}
+	public void setAbdominalCircumference(int abdominalCircumference) {
+		this.abdominalCircumference = abdominalCircumference;
+	}
+	public void setHumerusLength(int humerusLength) {
+		this.humerusLength = humerusLength;
+	}
+	public void setEstimatedFetalWeight(int estimateFetalWeight) {
+		this.estimatedFetalWeight = estimateFetalWeight;
+	}
+	
 }
