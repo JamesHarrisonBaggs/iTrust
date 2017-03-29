4c0f1fa711d07459e5c85d3041701d9364025bc1
ereinst <ereinst@ncsu.edu>
2017-03-29 13:11:29 -0400

    Added MySQLTest frameworks for Pregnancy and Ultrasound

---
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/unit/model/obgyn/PregnancyMySQLTest.java
@@ -0,0 +1,31 @@
+package edu.ncsu.csc.itrust.unit.model.obgyn;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class PregnancyMySQLTest {
+	
+	@Test
+	public void testPregnancyMySQL() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetByID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetByDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/unit/model/obgyn/UltrasoundMySQLTest.java
@@ -0,0 +1,46 @@
+package edu.ncsu.csc.itrust.unit.model.obgyn;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class UltrasoundMySQLTest {
+
+	@Test
+	public void testUltrasoundMySQL() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUltrasoundMySQLDataSource() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetByVisitFetus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetByPatientId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetByPatientIdVisitId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveUltrasound() {
+		fail("Not yet implemented");
+	}
+
+}
