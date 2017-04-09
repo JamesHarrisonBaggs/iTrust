cf72eecb0c5ecf7093ff3185fb3deffcaec10869
ereinst <ereinst@ncsu.edu>
2017-04-09 15:20:47 -0400

    Restored deleted code; re-added LaborReportController

---
+++ iTrust/src/main/edu/ncsu/csc/itrust/controller/obgyn/LaborReportController.java
@@ -0,0 +1,191 @@
+package edu.ncsu.csc.itrust.controller.obgyn;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.sql.DataSource;
+
+import edu.ncsu.csc.itrust.controller.iTrustController;
+import edu.ncsu.csc.itrust.exception.DBException;
+import edu.ncsu.csc.itrust.model.obgyn.ChildbirthMySQL;
+import edu.ncsu.csc.itrust.model.obgyn.ChildbirthVisitMySQL;
+import edu.ncsu.csc.itrust.model.obgyn.ObstetricsInit;
+import edu.ncsu.csc.itrust.model.obgyn.ObstetricsInitMySQL;
+import edu.ncsu.csc.itrust.model.obgyn.ObstetricsVisit;
+import edu.ncsu.csc.itrust.model.obgyn.ObstetricsVisitMySQL;
+import edu.ncsu.csc.itrust.model.obgyn.Pregnancy;
+import edu.ncsu.csc.itrust.model.obgyn.PregnancyFlag;
+import edu.ncsu.csc.itrust.model.obgyn.PregnancyMySQL;
+import edu.ncsu.csc.itrust.model.obgyn.UltrasoundMySQL;
+import edu.ncsu.csc.itrust.model.old.beans.AllergyBean;
+import edu.ncsu.csc.itrust.model.old.beans.PatientBean;
+import edu.ncsu.csc.itrust.model.old.dao.DAOFactory;
+import edu.ncsu.csc.itrust.model.old.dao.mysql.AllergyDAO;
+import edu.ncsu.csc.itrust.model.old.dao.mysql.PatientDAO;
+import edu.ncsu.csc.itrust.model.old.enums.TransactionType;
+import edu.ncsu.csc.itrust.webutils.SessionUtils;
+
+@ManagedBean(name="report_controller")
+@SessionScoped
+public class LaborReportController extends iTrustController {
+
+	/** OBGYN databases */
+	private ObstetricsInitMySQL initsDB;
+	private PregnancyMySQL pregnancyDB;
+	private ObstetricsVisitMySQL obVisitsDB;
+//	private UltrasoundMySQL ultrasoundsDB;
+//	private ChildbirthMySQL childbirthsDB;
+//	private ChildbirthVisitMySQL cbVisitsDB;
+	
+	/** Patient databases */
+	private PatientDAO patientDB;
+	private AllergyDAO allergyDB;
+	
+	/** Other data */
+	private long patientID;
+	private SessionUtils sessionUtils;
+	
+	/**
+	 * Constructs a LaborReportController
+	 */
+	public LaborReportController() throws DBException {
+		super();
+		sessionUtils = getSessionUtils();
+		assert(sessionUtils != null);
+		
+		// OBGYN databases
+		initsDB = new ObstetricsInitMySQL();
+		pregnancyDB = new PregnancyMySQL();
+		obVisitsDB = new ObstetricsVisitMySQL();
+//		ultrasoundsDB = new UltrasoundMySQL();
+//		childbirthsDB = new ChildbirthMySQL();
+//		cbVisitsDB = new ChildbirthVisitMySQL();
+
+		// patient databases
+		patientDB = DAOFactory.getProductionInstance().getPatientDAO();
+		allergyDB = DAOFactory.getProductionInstance().getAllergyDAO();
+		
+		// patient data
+		patientID = sessionUtils.getCurrentPatientMIDLong();
+
+	}
+	
+	/**
+	 * Constructs a LaborReportController with a Data Source
+	 */
+	public LaborReportController(DataSource ds) throws DBException {
+// 		TODO INCOMPLETE STILL NEED TO DO A LOT OF STUFF
+//		super();
+//		initsDB = new ObstetricsInitMySQL(ds);
+//		pregnancyDB = new PregnancyMySQL(ds);
+//		obVisitsDB = new ObstetricsVisitMySQL(ds);
+//		ultrasoundsDB = new UltrasoundMySQL(ds);
+//		childbirthsDB = new ChildbirthMySQL(ds);
+//		cbVisitsDB = new ChildbirthVisitMySQL(ds);
+	}
+		
+	// TODO handle and not just throw DBExceptions
+	
+	/**
+	 * Returns data to display
+	 */	
+	public List<ObstetricsInit> getObInits() throws DBException {
+		return initsDB.getByID(patientID);
+	}
+	public ObstetricsInit getCurrentInit() throws DBException {
+		return initsDB.getByID(patientID).get(0);
+	}
+	public List<ObstetricsVisit> getObVisits() throws DBException {
+		return obVisitsDB.getByID(patientID);
+	}	
+	public List<Pregnancy> getPregnancies() throws DBException {
+		return pregnancyDB.getByID(patientID);
+	}
+	public List<AllergyBean> getAllergies() throws DBException {
+		return allergyDB.getAllergies(patientID);
+	}
+	
+	/**
+	 * Returns the patient's blood type
+	 */
+	public String getBloodType() throws DBException {
+		PatientBean patient = patientDB.getPatient(patientID);
+		return patient.getBloodType().toString();
+	}
+	
+	/**
+	 * Returns a list of all Pregnancy Warning Flags
+	 */
+	public List<PregnancyFlag>getPregnancyWarningFlags() throws DBException {
+		
+		// information sources
+		List<ObstetricsInit> obInits = getObInits();
+		List<ObstetricsVisit> obVisits = getObVisits();
+		PatientBean patient = patientDB.getPatient(patientID);
+		
+		// all warning flags
+		boolean rhFlag = false;
+		boolean highBP = false;
+		boolean advancedAge = false;
+		boolean preExisting = false;
+		boolean allergies = false;
+		boolean lowLying = false;
+		boolean genMiscarriage = false;
+		boolean abnormalFHR = false;
+		boolean multiples = false;
+		boolean atypicalWeight = false;
+		boolean hGravidarum = false;
+		boolean hypothyroidism = false;
+		
+		// get flags from visit
+		for (ObstetricsVisit v : obVisits) {
+			if (v.isRhFlag()) rhFlag = true;
+			// high blood pressure?
+			if (v.isLowLyingPlacenta()) lowLying = true;
+			int fhr = v.getFetalHeartRate();
+			if (fhr < 120 || fhr > 160) abnormalFHR = true;
+			if (v.getAmount() > 1) multiples = true;
+		}
+		
+		// advanced age
+		Instant dateOfBirth = patient.getDateOfBirth().toInstant();
+		Instant expDueDate = obInits.get(0).getEstimatedDueDate().atStartOfDay().toInstant(ZoneOffset.UTC);		
+		long ageSec = expDueDate.getEpochSecond() - dateOfBirth.getEpochSecond();
+		long ageYears = ageSec / (60L * 60L * 24L * 365L);
+		if (ageYears >= 35) {
+			advancedAge = true;
+		}
+		
+		// TODO pre existing?
+		// TODO allergies?
+		// TODO genetic miscarriage?
+		// TODO atypical weight?
+		// TODO hyperemesis gravidarum?
+		// TODO hypothyroidism?
+
+		// assemble flag list
+		List<PregnancyFlag> flags = new ArrayList<PregnancyFlag>();
+		flags.add(new PregnancyFlag(rhFlag, "RH factor"));
+		flags.add(new PregnancyFlag(highBP, "High Blood Pressure"));
+		flags.add(new PregnancyFlag(advancedAge, "Advanced Maternal Age"));
+		flags.add(new PregnancyFlag(lowLying, "Low-Lying Placenta"));
+		flags.add(new PregnancyFlag(multiples, "Multiples"));
+		flags.add(new PregnancyFlag(abnormalFHR, "Abnormal Fetal Heart Rate"));
+		
+		return flags;
+	}
+		
+	// TODO get pre-existing conditions???
+	
+	/**
+	 * Logs the viewing of a report
+	 */
+	public void logReportView() {
+		logTransaction(TransactionType.LABOR_DELIVERY_REPORT, "");
+	}
+	
+}
--- iTrust/src/test/java/edu/ncsu/csc/itrust/unit/controller/obgyn/ChildbirthControllerTest.java
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/unit/controller/obgyn/ChildbirthControllerTest.java
@@ -21,15 +21,16 @@ import edu.ncsu.csc.itrust.unit.datagenerators.TestDataGenerator;
 import edu.ncsu.csc.itrust.webutils.SessionUtils;
 
 public class ChildbirthControllerTest {
-	
+
 	ChildbirthController controller;
 	DataSource ds;
 	TestDataGenerator gen;
-	
-	@Mock SessionUtils mockSessionUtils;
-	
+
+	@Mock
+	SessionUtils mockSessionUtils;
+
 	List<Childbirth> list;
-	Childbirth bean;	
+	Childbirth bean;
 
 	@Before
 	public void setUp() throws Exception {
@@ -49,60 +50,66 @@ public class ChildbirthControllerTest {
 	}
 
 	@Test
+	public void testGettersSetters() {
+		LocalDateTime date = LocalDateTime.of(2016, 03, 24, 1, 30);
+		controller.setBirthDate(date);
+		assertEquals(date, controller.getBirthDate());
+		controller.setBirthID(0);
+		controller.getBirthID();
+		controller.setDateOfBirth(date);
+		controller.getDateOfBirth();
+		controller.setDeliveryType("Vaginal Delivery");
+		controller.getDeliveryType();
+		controller.setEpiduralAnaesthesia(0);
+		controller.getEpiduralAnaesthesia();
+		controller.setGender("Male");
+		controller.getGender();
+		controller.setMagnesiumSO4(0);
+		controller.getMagnesiumSO4();
+		controller.setMid(1);
+		controller.getMid();
+		controller.setName("Eric");
+		controller.getName();
+		controller.setNitrousOxide(10);
+		controller.getNitrousOxide();
+		controller.setPethidine(10);
+		controller.getPethidine();
+		controller.setPid(1);
+		controller.getPid();
+		controller.setPitocin(10);
+		controller.getPitocin();
+		controller.setTimeOfBirth(LocalTime.now());
+		controller.getTimeOfBirth();
+		controller.setVisitDate(date);
+		controller.getVisitDate();
+		controller.setVisitId(20);
+		controller.getVisitId();
+	}
+
+	@Test
+	public void testBooleans() {
+		controller.setEditBaby(true);
+		controller.isEditBaby();
+		controller.setAdded(true);
+		controller.isAdded();
+		controller.setEstimated(true);
+		controller.isEstimated();
+		controller.setNewBaby(false);
+		controller.isNewBaby();
+		controller.setPreEstimateFlag(true);
+		controller.isPreEstimateFlag();
+		controller.setPreScheduled(true);
+		controller.isPreScheduled();
+	}
+
+	@Test
 	public void testChildbirthController() {
 		try {
-			LocalDateTime date = LocalDateTime.of(2016, 03, 24, 1, 30);
-			controller.setBirthDate(date);
-			assertEquals(date, controller.getBirthDate());
-			controller.setBirthID(0);
-			controller.getBirthID();
-			controller.setDateOfBirth(date);
-			controller.getDateOfBirth();
-			controller.setDeliveryType("Vaginal Delivery");
-			controller.getDeliveryType();
-			controller.setEpiduralAnaesthesia(0);
-			controller.getEpiduralAnaesthesia();
-			controller.setGender("Male");
-			controller.getGender();
-			controller.setMagnesiumSO4(0);
-			controller.getMagnesiumSO4();
-			controller.setMid(1);
-			controller.getMid();
-			controller.setName("Eric");
-			controller.getName();
-			controller.setNitrousOxide(10);
-			controller.getNitrousOxide();
-			controller.setPethidine(10);
-			controller.getPethidine();
-			controller.setPid(1);
-			controller.getPid();
-			controller.setPitocin(10);
-			controller.getPitocin();
-			controller.setTimeOfBirth(LocalTime.now());
-			controller.getTimeOfBirth();
-			controller.setVisitDate(date);
-			controller.getVisitDate();
-			controller.setVisitId(20);
-			controller.getVisitId();
-			// booleans
-			controller.setEditBaby(true);
-			controller.isEditBaby();
-			controller.setAdded(true);
-			controller.isAdded();
-			controller.setEstimated(true);
-			controller.isEstimated();
-			controller.setNewBaby(false);
-			controller.isNewBaby();
-			controller.setPreEstimateFlag(true);
-			controller.isPreEstimateFlag();
-			controller.setPreScheduled(true);
-			controller.isPreScheduled();
-			// ???
 			controller.submitNew();
 			controller.getChildbirthVisitID();
 			controller.getChildbirthVisitVisitID();
 		} catch (DBException e) {
-			System.err.println(e.getExtendedMessage());
+			System.out.println(e.getExtendedMessage());
 		}
 	}
 
