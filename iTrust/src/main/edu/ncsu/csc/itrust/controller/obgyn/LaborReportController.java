886382f1f4368a23678307c8a78bdc84b14f1f36
ereinst <ereinst@ncsu.edu>
2017-04-12 12:57:17 -0400

    Implemented LaborReportControllerTest

--- iTrust/src/main/edu/ncsu/csc/itrust/controller/obgyn/LaborReportController.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/controller/obgyn/LaborReportController.java
@@ -35,6 +35,7 @@ import edu.ncsu.csc.itrust.model.old.dao.DAOFactory;
 import edu.ncsu.csc.itrust.model.old.dao.mysql.AllergyDAO;
 import edu.ncsu.csc.itrust.model.old.dao.mysql.PatientDAO;
 import edu.ncsu.csc.itrust.model.old.enums.TransactionType;
+import edu.ncsu.csc.itrust.unit.testutils.TestDAOFactory;
 import edu.ncsu.csc.itrust.webutils.SessionUtils;
 
 @ManagedBean(name="report_controller")
@@ -47,14 +48,11 @@ public class LaborReportController extends iTrustController {
 	private DiagnosisMySQL diagnosisDB;
 	private ICDCodeMySQL icdcodeDB;
 	private OfficeVisitMySQL officevisitDB;
-//	private UltrasoundMySQL ultrasoundsDB;
-//	private ChildbirthMySQL childbirthsDB;
-//	private ChildbirthVisitMySQL cbVisitsDB;
 	
 	/** Patient databases */
 	private PatientDAO patientDB;
 	private AllergyDAO allergyDB;
-	
+	private DAOFactory factory;
 	
 	/** Other data */
 	private long patientID;
@@ -66,40 +64,40 @@ public class LaborReportController extends iTrustController {
 	public LaborReportController() throws DBException {
 		super();
 		sessionUtils = getSessionUtils();
-		assert(sessionUtils != null);
+		factory = DAOFactory.getProductionInstance();
 		
-		// OBGYN databases
 		initsDB = new ObstetricsInitMySQL();
 		pregnancyDB = new PregnancyMySQL();
 		obVisitsDB = new ObstetricsVisitMySQL();
 		diagnosisDB = new DiagnosisMySQL();
 		icdcodeDB = new ICDCodeMySQL();
 		officevisitDB = new OfficeVisitMySQL();
-//		ultrasoundsDB = new UltrasoundMySQL();
-//		childbirthsDB = new ChildbirthMySQL();
-//		cbVisitsDB = new ChildbirthVisitMySQL();
-
-		// patient databases
-		patientDB = DAOFactory.getProductionInstance().getPatientDAO();
-		allergyDB = DAOFactory.getProductionInstance().getAllergyDAO();
 		
-		// patient data
-		patientID = sessionUtils.getCurrentPatientMIDLong();
-
+		this.setPatientData();
 	}
 	
 	/**
 	 * Constructs a LaborReportController with a Data Source
 	 */
-	public LaborReportController(DataSource ds) throws DBException {
-// 		TODO INCOMPLETE STILL NEED TO DO A LOT OF STUFF
-//		super();
-//		initsDB = new ObstetricsInitMySQL(ds);
-//		pregnancyDB = new PregnancyMySQL(ds);
-//		obVisitsDB = new ObstetricsVisitMySQL(ds);
-//		ultrasoundsDB = new UltrasoundMySQL(ds);
-//		childbirthsDB = new ChildbirthMySQL(ds);
-//		cbVisitsDB = new ChildbirthVisitMySQL(ds);
+	public LaborReportController(DataSource ds, SessionUtils utils) throws DBException {
+		super();
+		sessionUtils = utils;
+		factory = TestDAOFactory.getTestInstance();
+		
+		initsDB = new ObstetricsInitMySQL(ds);
+		pregnancyDB = new PregnancyMySQL(ds);
+		obVisitsDB = new ObstetricsVisitMySQL(ds);
+		diagnosisDB = new DiagnosisMySQL(ds);
+		icdcodeDB = new ICDCodeMySQL(ds);
+		officevisitDB = new OfficeVisitMySQL(ds);
+		
+		this.setPatientData();
+	}
+	
+	private void setPatientData() {
+		patientID = sessionUtils.getCurrentPatientMIDLong();
+		patientDB = factory.getPatientDAO();
+		allergyDB = factory.getAllergyDAO();
 	}
 		
 	// TODO handle and not just throw DBExceptions
@@ -111,8 +109,7 @@ public class LaborReportController extends iTrustController {
 		return initsDB.getByID(patientID);
 	}
 	
-	public boolean reportAvailable() throws DBException{
-		
+	public boolean reportAvailable() throws DBException {
 		boolean isAvailable = true;
 		PatientBean patient = patientDB.getPatient(patientID);
 		if(!patient.isObgynEligible()){
@@ -122,18 +119,23 @@ public class LaborReportController extends iTrustController {
 		}
 		return isAvailable;
 	}
+	
 	public ObstetricsInit getCurrentInit() throws DBException {
 		return initsDB.getByID(patientID).get(0);
 	}
+	
 	public List<ObstetricsVisit> getObVisits() throws DBException {
 		return obVisitsDB.getByID(patientID);
 	}	
+	
 	public List<Pregnancy> getPregnancies() throws DBException {
 		return pregnancyDB.getByID(patientID);
 	}
+	
 	public List<AllergyBean> getAllergies() throws DBException {
 		return allergyDB.getPregnancyRelatedAllergies(patientID);
 	}
+	
 	public LocalDate getMostRecentEDD() throws DBException{
 		ObstetricsInit recent = getCurrentInit();
 		return recent.getEstimatedDueDate();
@@ -152,7 +154,7 @@ public class LaborReportController extends iTrustController {
 		List<ICDCode> diagnosiscodes = new ArrayList<ICDCode>(); 
 		for(OfficeVisit v : visits){
 			List<Diagnosis> diags = diagnosisDB.getAllDiagnosisByOfficeVisit(v.getVisitID());
-			for(Diagnosis d : diags){
+			for (Diagnosis d : diags){
 				ICDCode icd = null;
 				try {
 					icd = icdcodeDB.getByCode(d.getCode());
@@ -160,7 +162,7 @@ public class LaborReportController extends iTrustController {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				if(icd.isChronic() || icd.isPregnancyRelated()){
+				if (icd.isChronic() || icd.isPregnancyRelated()){
 					diagnosiscodes.add(icd);
 				}
 			}
@@ -174,24 +176,24 @@ public class LaborReportController extends iTrustController {
 		//officevisitDB
 	}
 	
-	public List<String> getPregnancyWarningFlagsForVisit(ObstetricsVisit obvisit){
+	public List<String> getPregnancyWarningFlagsForVisit(ObstetricsVisit obvisit) {
 		List<String> flags = new ArrayList<String>();
 		String vid = Long.toString(obvisit.getVisitId());
-		if(obvisit.isRhFlag()){
+		if (obvisit.isRhFlag()){
 			flags.add("Visit " + vid + " has RH Flag set!");
 		}
 		String bp = obvisit.getBloodPressure();
 		String parts[] = bp.split("/");			
-		if(Integer.parseInt(parts[0]) >= 140 || Integer.parseInt(parts[1]) >= 90){
+		if (Integer.parseInt(parts[0]) >= 140 || Integer.parseInt(parts[1]) >= 90){
 			flags.add("Visit " + vid + " has High Blood Pressure");
 		}
-		if(obvisit.isLowLyingPlacenta()){
+		if (obvisit.isLowLyingPlacenta()){
 			flags.add("Visit " + vid + " has Low Lying Placenta");
 		}
-		if(obvisit.getFetalHeartRate() > 160 || obvisit.getFetalHeartRate() < 120){
-			flags.add("Visit " + vid + " has Abnormal Fetal Heartrate!");
+		if (obvisit.getFetalHeartRate() > 160 || obvisit.getFetalHeartRate() < 120){
+			flags.add("Visit " + vid + " has Abnormal Fetal Heartrate");
 		}
-		if(obvisit.getAmount() > 1){
+		if (obvisit.getAmount() > 1){
 			flags.add("Visit " + vid + " has Multiples in pregnancy");
 		}
 		
---
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/unit/controller/obgyn/LaborReportControllerTest.java
@@ -0,0 +1,47 @@
+package edu.ncsu.csc.itrust.unit.controller.obgyn;
+
+import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import edu.ncsu.csc.itrust.controller.obgyn.LaborReportController;
+import edu.ncsu.csc.itrust.model.ConverterDAO;
+import edu.ncsu.csc.itrust.webutils.SessionUtils;
+
+public class LaborReportControllerTest {
+	
+	LaborReportController controller;
+	DataSource ds;
+	@Mock SessionUtils mockSessionUtils;
+	
+	@Before
+	public void setUp() throws Exception {
+		ds = ConverterDAO.getDataSource();
+		mockSessionUtils = Mockito.mock(SessionUtils.class);
+		Mockito.doReturn(2L).when(mockSessionUtils).getCurrentPatientMIDLong();
+		controller = new LaborReportController(ds, mockSessionUtils);
+	}
+
+	@Test
+	public void testLaborReportController() throws Exception {
+		controller.getObInits();
+		controller.reportAvailable();
+		controller.getCurrentInit();
+		controller.getObVisits();
+		controller.getPregnancies();
+		controller.getAllergies();
+		controller.getMostRecentEDD();
+		controller.getBloodType();
+		controller.getRelevantConditions();
+//		controller.getPregnancyWarningFlagsForVisit(obvisit);
+		controller.getAllPregnancyWarningFlags();
+//		controller.getComplication(visit);
+		controller.logReportView();
+	}
+
+}
