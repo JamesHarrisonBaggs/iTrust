84e78de5fc6f76b1186837feb0b8e011736b4421
ereinst <ereinst@ncsu.edu>
2017-04-13 18:28:10 -0400

    Refactored duplicate code in UserController

--- iTrust/src/main/edu/ncsu/csc/itrust/controller/user/UserController.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/controller/user/UserController.java
@@ -11,76 +11,51 @@ import edu.ncsu.csc.itrust.model.user.UserMySQLConverter;
 @ManagedBean(name="user")
 public class UserController {
 	private DataBean<User> userData;
+	
 	public UserController() throws DBException{
-		userData = new UserMySQLConverter();
-		
+		userData = new UserMySQLConverter();	
 	}
 	
-	public String getUserNameForID(String mid) throws DBException{
-		User user = null;
-		if( mid == null) return "";
-		if(mid.isEmpty()) return "";
-		long id = -1;
-		try{
-			id = Long.parseLong(mid);
-		}
-		catch(NumberFormatException ne){
-			return "";
-		}
-		//if(id<1) return "";
-		user = userData.getByID(id);
-		if(user != null){
-			if(user.getRole().equals(Role.TESTER)){
-				return Long.toString(user.getMID());
-			}
-			else{
-				return user.getLastName().concat(", "+user.getFirstName());
-			}
-			
-		}
-		else{
-			return "";
-		}
-		
+	public String getUserNameForID(String mid) throws DBException {
+		User user = getUser(mid, true);
+		if (user == null) return "";
+		if (user.getRole().equals(Role.TESTER)){
+			return Long.toString(user.getMID());
+		} else {
+			return user.getLastName().concat(", " + user.getFirstName());
+		}			
 	}
-	public String getUserRoleForID(String mid) throws DBException{
-		User user = null;
-		if( mid == null) return "";
-		if(mid.isEmpty()) return "";
-		long id = -1;
-		try{
-			id = Long.parseLong(mid);
-		}
-		catch(NumberFormatException ne){
-			return "";
-		}
-		if(id<1) return "";
-		user = userData.getByID(id);
+
+	public String getUserRoleForID(String mid) throws DBException {
+		User user = getUser(mid, false);
+		if (user == null) return "";
 		return user.getRole().getUserRolesString().toLowerCase();
 	}
 	
 	public boolean doesUserExistWithID(String mid) throws DBException{
-		User user = null;
-		if( mid == null) return false;
-		long id = -1;
-		try{
-			id = Long.parseLong(mid);
-		}
-		catch(NumberFormatException ne){
-			return false;
-		}
-		user = userData.getByID(id);
-		if(!(user == null)){
-				return true;
-		}
-		else{
-			return false;
-		}
-
-		
+		User user = getUser(mid, true);
+		return user != null;
 	}
-	
-	
-	
+
+	/**
+	 * Returns a User associated with an MID
+	 * Returns null for an invalid MID or missing patient
+	 * @param allowInvalidMIDs - true to search for users with MID < 1
+	 */
+	private User getUser(String mid, boolean allowInvalidMIDs) throws DBException {
+		if (mid == null || mid.isEmpty()) {
+			return null;
+		}
+		long id = -1;
+		try {
+			id = Long.parseLong(mid);
+		} catch (NumberFormatException n) {
+			return null;
+		}
+		if (!allowInvalidMIDs && id < 1) {
+			return null;
+		}
+		return userData.getByID(id);
+	}
 
 }
